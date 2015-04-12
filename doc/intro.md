ed8b4fb9dc56452026f22b9627979e97ae08c637
Paul Dumais <paul@unstate.ca>
2015-04-12 17:09:25 -0600

    first

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to telehash-packet
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/telehash_packet/core_test.clj
@@ -0,0 +1,7 @@
+(ns telehash-packet.core-test
+  (:require [clojure.test :refer :all]
+            [telehash-packet.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
