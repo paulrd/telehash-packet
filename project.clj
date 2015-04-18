e3b0e5f3f61c34c724427cdc1a9960476a42dc04
Paul Dumais <paul@unstate.ca>
2015-04-17 23:07:41 -0600

    Update Readme, release verion

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject telehash-packet "0.1.0-SNAPSHOT"
+(defproject telehash-packet "0.1.0"
   :description "A Clojure implementation of the Telehash Packet / LOB encoding protocol"
   :url "http://telehash.org"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,4 @@
   :dependencies [[commons-codec "1.10"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [funcool/octet "0.1.0"]
                  [gloss "0.2.4"]])
