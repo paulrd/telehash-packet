a77a5670d13062d6805a49f74860ee2f837f87c7
Paul Dumais <paul@unstate.ca>
2015-04-17 22:58:42 -0600

    Working

--- src/telehash_packet/core.clj
+++ src/telehash_packet/core.clj
@@ -4,7 +4,7 @@
             [gloss.core :as gc]
             [gloss.io :as gio]))
 
-(defn put-string [s]
+(defn to-byte-buffer [s]
   (gio/to-byte-buffer s))
 
 (defn encode 
@@ -24,71 +24,31 @@
 (defn decode
   "takes a ByteBuffer; see telehash.org protocol for packet format"
   [buf]
-  (let [fr [(gc/finite-frame :uint16 (gc/string :utf-8)) 
-            (gc/repeated :byte :prefix :none)]
+  (let [fr [(gc/header :uint16 #(if (< % 7) 
+                                 (gc/compile-frame (into [] (repeat % :byte)))
+                                 (gc/string :utf-8 :length %))
+                       identity)
+           (gc/repeated :byte :prefix :none)]
         res (gio/decode fr buf)]
-    {:json (-> res first (#(json/read-str % :key-fn keyword))) :body (last res)}))
+    {:head (-> res first (#(when % 
+                             (if (vector? %)
+                               %
+                               (json/read-str % :key-fn keyword)))))
+     :body (last res)}))
 
-(comment
-  (type (byte-array (map byte "hoolo")))
-  (= (Class/forName "[B") (type (byte-array (map byte "hoolo"))))
-  
-  (isa? (type {:hi "tru"}) PersistentArrayMap)
-  (type {:hi "tru"})
-  (json/write-str {:type "test" :foo ["bar"]})
-  (short (char 35))
+(defn packet? [p]
+  (and (map? p) (> (count p) 1) (or (vector? (:head p)) (map? (:head p)))
+       (vector? (:body p))))
 
-  (def fr (gc/compile-frame {:a :int16, :b :float32}))
-  (def fr (gc/compile-frame :int32))
 
-  (def ec (gio/encode :int32 3))
-  (def dc (gio/decode :int32 ec))
-  (-> ec first)
-  dc
 
-  (def fr (gc/compile-frame {:len :uint16}))
-  (def ec (gio/encode fr {:len 60000}))
-  (def dc (gio/decode fr ec))
-  dc
-  (.get (first ec))
-  (.flip (first ec))
 
-  (def fr (gc/compile-frame [:uint16-be (string :utf-8 :length )]))
-  (def ec (gio/encode fr [60000]))
-  (def dc (gio/decode fr ec))
-  dc
-  (.get (first ec))
-  (.flip (first ec))
-  
-  (def fr [(gc/finite-frame :uint16 (gc/string :utf-8)) 
-           (gc/repeated :byte :prefix :none)])
-  (def ec (gio/encode fr ["hello" "any binary!"]))
-  (def dc (gio/decode fr ec))
-  dc
-  ec
-  (.get (first ec))
-  (.get (last ec))
-  (.flip (first ec))
 
-  (let [ec (encode {:type "test" :foo ["bar"]} (put-string "any binary!"))
-        fr [(gc/finite-frame :uint16 (gc/string :utf-8)) 
-            (gc/repeated :byte :prefix :none)]]
-    (gio/decode fr ec)
-    )
-  
-  (def res (decode (encode {:type "test" :foo ["bar"]} (put-string "any binary!"))))
-  (:body res)
-  (def enc (encode {:type "test" :foo ["bar"]} (put-string "any binary!")))
-  (.get enc)
 
-  (let [hex-str "001d7b2274797065223a2274657374222c22666f6f223a5b22626172225d7d616e792062696e61727921"
-        bin (-> hex-str char-array 
-                org.apache.commons.codec.binary.Hex/decodeHex
-                java.nio.ByteBuffer/wrap)
-        packet (decode bin)]
-    packet
-    )
-  
-)
+
+
+
+
+
 
 
--- test/telehash_packet/core_test.clj
+++ test/telehash_packet/core_test.clj
@@ -5,7 +5,7 @@
 (deftest should-encode
   (testing "encode, check return type, check length"
     (let [json {:type "test" :foo ["bar"]}
-          body (put-string "any binary!")
+          body (to-byte-buffer "any binary!")
           bin (encode json body)]
       (is (isa? (type bin) java.nio.ByteBuffer))
       (is (= 42 (.limit bin))))))
@@ -18,7 +18,7 @@
                  java.nio.ByteBuffer/wrap)
           packet (decode bin)]
       (is (isa? (type packet) clojure.lang.IPersistentMap))
-      (is (= (:type (:json packet)) "test"))
+      (is (= (:type (:head packet)) "test"))
       (is (= (count (:body packet)) 11)))))
 
 (deftest should-transpose
@@ -29,11 +29,48 @@
                  java.nio.ByteBuffer/wrap)
           packet (decode bin)
           body (-> packet :body byte-array java.nio.ByteBuffer/wrap)
-          bin (encode (:json packet) body)]
+          bin (encode (:head packet) body)]
       (is (isa? (type bin) java.nio.ByteBuffer))
       (is (= hex-str (org.apache.commons.codec.binary.Hex/encodeHexString 
                       (.array bin)))))))
 
+(deftest should-handle-no-head
+  (testing "encode/decode, return type and length; check body content"
+    (let [body (to-byte-buffer "any binary!")
+          bin (encode nil body)
+          packet (decode bin)]
+      (is (isa? (type bin) java.nio.ByteBuffer))
+      (is (= 13 (.limit bin)))
+      (is (empty? (:head packet)))
+      (is (= (apply str (map char (:body packet))) "any binary!")))))
+
+(deftest should-empty-head-and-body
+  (testing "encode, check length"
+    (let [bin (encode nil nil)]
+      (is (= 2 (.limit bin))))))
+
+(deftest should-handle-binary-head
+  (testing "encode/decode: check type, length, content"
+    (let [head (to-byte-buffer 0x42)
+          body (to-byte-buffer "any binary!")
+          bin (encode head body)
+          packet (decode bin)]
+      (is (isa? (type bin) java.nio.ByteBuffer))
+      (is (= 14 (.limit bin)))
+      (is (= 1 (count (:head packet)))))))
+
+(deftest should-verify
+  (testing "encode and verify packet"
+    (let [hex-str "001d7b2274797065223a2274657374222c22666f6f223a5b22626172225d7d616e792062696e61727921"
+          bin (-> hex-str char-array 
+                 org.apache.commons.codec.binary.Hex/decodeHex
+                 java.nio.ByteBuffer/wrap)
+          packet (decode bin)]
+      (is (packet? packet))
+      (is (not (packet? {})))
+      (is (not (packet? [])))
+      (is (packet? {:head {} :body []}))
+      (is (not (packet {:head nil :body []}))))))
 
 
 
